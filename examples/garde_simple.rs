724c67d947dc5d25a5402a0aa04df84864c07769
ranger-ross <rosssullivan101@gmail.com>
2024-08-03 12:12:01 +0900

    Added garde example

---
+++ examples/garde_simple.rs
@@ -0,0 +1,34 @@
+/// To run this example use ``cargo r --example garde_simple --features garde`
+///
+/// Once the server is running you can test with
+/// ```
+/// curl -X POST localhost:8080/example --json '{"name": "foo"}'
+/// ```
+///
+/// Changing the length of the name should result to more than 4 chars should result in HTTP 200
+///
+use actix_web::{post, web::Json, App, HttpResponse, HttpServer, Responder};
+use actix_web_validation::Validated;
+use garde::Validate;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize, Validate)]
+struct Example {
+    #[garde(length(min = 4))]
+    name: String,
+}
+
+#[post("/example")]
+async fn example(Validated(Json(payload)): Validated<Json<Example>>) -> impl Responder {
+    println!("Got validated payload {:#?}", payload);
+
+    HttpResponse::Ok().body(format!("Hello {}", payload.name))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(example))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
