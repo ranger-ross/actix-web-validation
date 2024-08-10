6d8202e9c11dea69be616a52de1bc8f407a19950
Ross <rosssullivan101@gmail.com>
2024-08-10 21:55:27 +0900

    Added trait def macro

--- src/lib.rs
+++ src/lib.rs
@@ -15,3 +15,39 @@ pub use crate::garde::Validated;
 
 #[cfg(all(feature = "custom", not(feature = "validator"), not(feature = "garde")))]
 pub use crate::custom::Validated;
+
+macro_rules! validated_definition {
+    () => {
+        impl<T> Validated<T> {
+            pub fn into_inner(self) -> T {
+                self.0
+            }
+        }
+
+        impl<T> std::ops::Deref for Validated<T> {
+            type Target = T;
+
+            fn deref(&self) -> &Self::Target {
+                &self.0
+            }
+        }
+
+        impl<T> std::ops::DerefMut for Validated<T> {
+            fn deref_mut(&mut self) -> &mut Self::Target {
+                &mut self.0
+            }
+        }
+
+        impl<T> Debug for Validated<T>
+        where
+            T: Debug,
+        {
+            #[inline]
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                f.debug_tuple("Validated").field(&self.0).finish()
+            }
+        }
+    };
+}
+
+pub(crate) use validated_definition;
